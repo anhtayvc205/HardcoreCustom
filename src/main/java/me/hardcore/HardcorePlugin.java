c5136d78988828449badb7bc45ec04dea7a8e952
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-12 21:37:54 +0700

    Update HardcorePlugin.java

--- src/main/java/me/hardcore/HardcorePlugin.java
+++ src/main/java/me/hardcore/HardcorePlugin.java
@@ -1,51 +1,49 @@
 package me.hardcore;
 
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
+import me.hardcore.gui.ConfirmGUI;
+import me.hardcore.gui.ReviveGUI;
+import org.bukkit.*;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class HardcorePlugin extends JavaPlugin {
 
     private static HardcorePlugin instance;
     private File dataFile;
-    private FileConfiguration data;
-
-    @Override
-    public void onEnable() {
-        instance = this;
-        saveDefaultConfig();
-        loadData();
-
-        getServer().getPluginManager().registerEvents(new DeathListener(), this);
-        getCommand("hoisinh").setExecutor(new ReviveCommand());
-        getCommand("hardcore").setExecutor(new ReviveCommand());
-
-        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            new HardcorePlaceholder().register();
-        }
-    }
-
-    @Override
-    public void onDisable() {
-        saveData();
-    }
+    private YamlConfiguration data;
 
     public static HardcorePlugin getInstance() {
         return instance;
     }
 
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        saveDefaultConfig();
+        loadData();
+
+        Bukkit.getPluginManager().registerEvents(new DeathListener(), this);
+        Bukkit.getPluginManager().registerEvents(new ReviveGUI(), this);
+        Bukkit.getPluginManager().registerEvents(new ConfirmGUI(), this);
+
+        getCommand("hoisinh").setExecutor(new ReviveCommand());
+        getCommand("hardcore").setExecutor(new ReviveCommand());
+
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new HardcorePlaceholder(this).register();
+        }
+
+        getLogger().info("HardcoreCustom enabled");
+    }
+
     public void loadData() {
         dataFile = new File(getDataFolder(), "data.yml");
-        if (!dataFile.exists()) {
-            dataFile.getParentFile().mkdirs();
-            saveResource("data.yml", false);
-        }
+        if (!dataFile.exists()) saveResource("data.yml", false);
         data = YamlConfiguration.loadConfiguration(dataFile);
     }
 
@@ -58,11 +56,11 @@ public class HardcorePlugin extends JavaPlugin {
     }
 
     public int getLives(UUID uuid) {
-        return data.getInt("players." + uuid, 5);
+        return data.getInt("players." + uuid + ".lives", getConfig().getInt("max-lives"));
     }
 
     public void setLives(UUID uuid, int lives) {
-        data.set("players." + uuid, lives);
+        data.set("players." + uuid + ".lives", lives);
         saveData();
     }
 
@@ -70,7 +68,7 @@ public class HardcorePlugin extends JavaPlugin {
         return getConfig().getConfigurationSection("revive-cost").getValues(false);
     }
 
-    public boolean isEffectEnabled(String key) {
-        return getConfig().getBoolean("revive." + key, true);
+    public boolean isEffect(String key) {
+        return getConfig().getBoolean("effects." + key);
     }
 }
