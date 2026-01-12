478e47b90f10480f8ec9d95baeedef04130d5b2a
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-12 20:30:12 +0700

    Update HardcorePlaceholder.java

--- src/main/java/me/hardcore/HardcorePlaceholder.java
+++ src/main/java/me/hardcore/HardcorePlaceholder.java
@@ -11,11 +11,25 @@ public class HardcorePlaceholder extends PlaceholderExpansion {
     }
 
     @Override
-    public String onPlaceholderRequest(Player p, String params) {
-        if (p == null) return "";
+    public String getAuthor() {
+        return "HardcoreCustom";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.3";
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player, String params) {
+        if (player == null) return "";
+
         if (params.equalsIgnoreCase("lives")) {
-            return String.valueOf(HardcorePlugin.getInstance().getLives(p.getUniqueId()));
+            return String.valueOf(
+                HardcorePlugin.getInstance().getLives(player.getUniqueId())
+            );
         }
+
         return null;
     }
 }
